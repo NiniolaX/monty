de3b9175d16107afe9a5050aa4331abc64fe469d
NiniolaX <loveafinni16@gmail.com>
2023-08-27 04:42:43 -0700

    Corrected error message for pint on empty stack command

--- pint.c
+++ pint.c
@@ -14,7 +14,7 @@ void pint(stack_t **stack, unsigned int line_number)
 	/* Check if stack is empty */
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%u: : can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
